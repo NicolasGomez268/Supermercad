a447c92c05488fc51ef023e07b41a0b76b402e50
NicolasGomez268 <cei.uen13@gmail.com>
2025-02-13 13:00:03 -0300

    Actualiza URLs para configurar vista inicial

--- productos/urls.py
+++ productos/urls.py
@@ -6,7 +6,7 @@ def redirect_to_guardar(request):
     return redirect('agregar_producto')
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.tu_vista, name='tu_vista'),
     path('productos_proximos/', views.productos_proximos, name='productos_proximos'),
     path('guardar_productos/', views.guardar_productos, name='agregar_producto'),
 ] 
